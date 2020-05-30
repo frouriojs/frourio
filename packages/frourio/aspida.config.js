75d73e3e15ad1fe0d07a03105cb1c8746bfd4272
m-mitsuhide <genbu2011@live.jp>
2020-05-30 15:58:09 +0900

    feat(config): rename aspida to frourio

--- packages/frourio/aspida.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  server: {
-    port: 10000,
-    basePath: '/api',
-    staticDir: 'packages/frourio/public',
-    cors: true,
-    immediate: false
-  }
-}
--- packages/frourio/src/getConfig.ts
+++ packages/frourio/src/getConfig.ts
@@ -1,4 +1,5 @@
-import getBaseConfig, { BaseConfig } from 'aspida/dist/getConfig'
+import fs from 'fs'
+import path from 'path'
 
 export type Config = {
   input: string
@@ -14,38 +15,36 @@ export type Config = {
   }
 }
 
-type ConfigFile = BaseConfig & {
-  server?: {
-    port?: number
-    basePath?: string
-    staticDir?: string | string[]
-    helmet?: boolean
-    cors?: boolean
-    immediate?: boolean
-    uploader?: {
-      dest?: string
-      size?: number
-    }
+type ConfigFile = {
+  input?: string
+  port?: number
+  basePath?: string
+  staticDir?: string | string[]
+  helmet?: boolean
+  cors?: boolean
+  immediate?: boolean
+  uploader?: {
+    dest?: string
+    size?: number
   }
 }
 
-const createConfig = (config: ConfigFile) => {
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  const server = config.server!
+const createConfig = (config: ConfigFile = {}): Config => ({
+  input: config.input ?? 'apis',
+  port: config.port ?? 8080,
+  basePath: config.basePath ?? '/',
+  staticDir: typeof config.staticDir === 'string' ? [config.staticDir] : config.staticDir,
+  helmet: config.helmet ?? true,
+  cors: config.cors ?? false,
+  immediate: config.immediate ?? true,
+  uploader: config.uploader ?? {}
+})
 
-  return {
-    input: config.input,
-    port: server.port ?? 8080,
-    basePath: server.basePath || '/',
-    staticDir: typeof server.staticDir === 'string' ? [server.staticDir] : server.staticDir,
-    helmet: server.helmet ?? true,
-    cors: server.cors ?? false,
-    immediate: server.immediate ?? true,
-    uploader: server.uploader ?? {}
-  }
+export default (configPath = 'frourio.config.js'): Config[] => {
+  if (!fs.existsSync(configPath)) return [createConfig()]
+
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const config: ConfigFile | ConfigFile[] = require(path.join(process.cwd(), configPath))
+
+  return Array.isArray(config) ? config.map(createConfig) : [createConfig(config)]
 }
-
-export default (configPath?: string): Config[] =>
-  getBaseConfig(configPath)
-    .filter((c: ConfigFile) => c.server)
-    .map(c => createConfig(c))
