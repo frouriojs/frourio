11c84d757378e7bfc8e55c8c7f9bc1a1e597e38d
solufa <solufa2020@gmail.com>
2020-07-30 23:42:46 +0900

    feat: update aspida@0.20.2

--- packages/frourio/__test__/build.spec.ts
+++ packages/frourio/__test__/build.spec.ts
@@ -1,8 +1,20 @@
 import fs from 'fs'
+import { version } from '../package.json'
+import { run } from '../src/cli'
 import build from '../src/buildServerFile'
 import aspidaBuild from 'aspida/dist/buildTemplate'
 
 const basePath = 'packages/frourio'
+
+test('version command', () => {
+  const spyLog = jest.spyOn(console, 'log')
+  const args = ['--version']
+
+  run(args)
+  expect(console.log).toHaveBeenCalledWith(`v${version}`)
+
+  spyLog.mockRestore()
+})
 
 test('build', () => {
   const inputDir = `${basePath}/server`
