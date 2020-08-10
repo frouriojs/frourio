09e5a91a0e0a3f9fb83b00dec92bec23f7f341dc
solufa <solufa2020@gmail.com>
2020-08-11 05:53:32 +0900

    chore: fix jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,7 @@
-const { pathsToModuleNameMapper } = require('ts-jest/utils')
-const { compilerOptions } = require('./tsconfig')
-
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   globals: { Blob: {} },
   testPathIgnorePatterns: ['apis'],
-  coveragePathIgnorePatterns: ['apis', 'dist'],
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-    prefix: '<rootDir>/'
-  })
+  coveragePathIgnorePatterns: ['apis', 'dist']
 }
